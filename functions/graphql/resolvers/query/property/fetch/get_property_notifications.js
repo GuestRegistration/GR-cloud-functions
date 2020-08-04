387d36f209999d8a4f6fc201b8d925bfe49f43e8
Adedayo Matthew <adedayomatt@gmail.com>
2020-08-04 13:43:01 +0100

    Fixed host properties notification

--- functions/graphql/resolvers/query/property/fetch/get_property_notifications.js
+++ functions/graphql/resolvers/query/property/fetch/get_property_notifications.js
@@ -5,34 +5,47 @@ const collections = require('../../../../../enums/collections')
 const client_middleware = require('../../../../middleware/client_authorized')
 const auth_middleware = require('../../../../middleware/user_authenticated')
 const admin = require('../../../../../admin')
+const { property } = require('../../../../../enums/collections')
 const firestore = admin.firestore()
 
  const getPropertyNotifications = async (parent, args, context) => { 
     let notifications = [];
-   
+
     client_middleware(context);
-    user_id = auth_middleware(context);
+    user_id = auth_middleware(context)
 
     if(user_id){
         const QuerySnapshots = await firestore.collection(collections.property.main).where('user_id', '==', user_id).get()
-        
-        QuerySnapshots.forEach(async (snapshot) => {
+        const notificationPromises = [];
+
+        QuerySnapshots.forEach(snapshot => {
             let property = snapshot.data();
             property.id = snapshot.ref.id;
-            let propertyNotifications = await firestore.collection(collections.property.main).doc(property.id)
-                .collection(collections.property.subcollections.notifications).get();
+            notificationPromises.push(getNotifications(property));
+        });
 
-            propertyNotifications.forEach((notificationSnapshot) => {
-                let notification = notificationSnapshot.data()
-                notification = notificationSnapshot.data();
-                notification.id = notificationSnapshot.ref.id;
-                notification.property = property;
-                notifications.push(notification)
-            })  
-        })        
+        (await Promise.all(notificationPromises))
+        .forEach(response => {
+            response.snapshot.forEach( notification => {
+                notifications.push({
+                    ...notification.data(),
+                    property: response.property
+                })
+            })
+        });
     }
 
-    return notifications;
+    return notifications;           
+ }
+
+ const getNotifications = (property) => {
+    return new Promise((resolve, reject) => {
+        firestore.collection(collections.property.main).doc(property.id)
+        .collection(collections.property.subcollections.notifications).get()
+        .then(snapshot => {
+            resolve({snapshot, property});
+        })
+    });
  }
 
  module.exports = getPropertyNotifications;

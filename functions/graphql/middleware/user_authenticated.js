1257a18192d3ef2a36a2968517bfdf49dee25f09
Adedayo Matthew <adedayomatt@gmail.com>
2020-08-04 14:22:49 +0100

    Checkout for valid user token first before attempting test user in user authentication middleware

--- functions/graphql/middleware/user_authenticated.js
+++ functions/graphql/middleware/user_authenticated.js
@@ -5,9 +5,9 @@
 
 const config = require('./../config')
 const UserNotAuthenticatedError = require('../error/UserNotAuthenticated')
-module.exports = (context) => {
-    if(context.auth.test_user) return context.auth.test_user;
+module.exports = (context) => {    
     if(!context.auth.user_token_valid){
+        if(context.auth.test_user) return context.auth.test_user;
         throw new UserNotAuthenticatedError
     }else{
         return context.auth.user_uid

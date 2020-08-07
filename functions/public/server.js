975d2c77d9b70f7ec3bcc5caaf1583da5c4b95ba
Adedayo Matthew <adedayomatt@gmail.com>
2020-08-07 11:57:23 +0100

    Published graphQL schema doc.

--- functions/public/server.js
+++ functions/public/server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require("express");
 const server = express();
 
-server.get('*', (req, res) => {
+server.get('/*', (req, res) => {
     const path = __dirname + '/htdocs' + req.path;
     fs.access(path, fs.F_OK, (err) => {
     if (err) {
@@ -11,7 +11,5 @@ server.get('*', (req, res) => {
     res.status(200).sendFile(path);
   });
 });
-
-
 
 module.exports = server;
